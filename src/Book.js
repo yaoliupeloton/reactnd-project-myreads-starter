f071f44b2e5a3199e701f63f9de8485187b814fa
yaoliupeloton <yao.liu@pelotoncycle.com>
2019-01-20 15:35:06 -0500

    changed Book.js to static function

--- src/Book.js
+++ src/Book.js
@@ -1,37 +1,25 @@
-import React, {Component} from 'react'
+import React from 'react'
 
-class Book extends Component {
-
-    // constructor(props) {
-    //     super(props);
-    //     this.handleChange = this.handleChange.bind(this);
-    // }
-    //
-    // handleChange(event) {
-    //     this.props.onCategoryChange(event.target.value);
-    // }
-
-    render() {
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover"
-                         style={{ backgroundImage: `url(${this.props.imageUrl})` }} />
-                    <div className="book-shelf-changer">
-                        <select value={this.props.category} onChange={this.props.onCategoryChange}>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none" disabled>None</option>
-                        </select>
-                    </div>
+function Book(props) {
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover"
+                     style={{ backgroundImage: `url(${props.imageUrl})` }} />
+                <div className="book-shelf-changer">
+                    <select value={props.category} onChange={props.onCategoryChange}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none" disabled>None</option>
+                    </select>
                 </div>
-                <div className="book-title">{this.props.title}</div>
-                <div className="book-authors">{this.props.author}</div>
             </div>
-        );
-    }
+            <div className="book-title">{props.title}</div>
+            <div className="book-authors">{props.author}</div>
+        </div>
+    );
 }
 
 export default Book
