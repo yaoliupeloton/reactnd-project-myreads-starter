917b88173bf27f28e1c8fd6acfe7ba0feaf71d81
yaoliupeloton <yao.liu@pelotoncycle.com>
2019-01-20 21:36:19 -0500

    search with api works. no UI yet

--- src/Search.js
+++ src/Search.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import * as BooksAPI from './BooksAPI';
+import Book from "./Shelf";
 
 class Search extends React.Component {
+
+    state = {
+        query: '',
+        searchRes: [],
+    };
+
+    updateQuery = (query) => {
+        console.log(`updated query: ${query}`);
+        this.setState({ query: query.trim()});
+
+        BooksAPI.search(query).then(books => {
+            this.setState({
+                searchRes: books.filter(it => it.imageLinks && it.imageLinks.thumbnail && it.authors && it.authors.length > 0),
+            });
+        });
+    };
+
     render() {
+
+        const {query, searchRes} = this.state;
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to='/' className="close-search" />
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author"/>
+                        <input type="text"
+                               placeholder="Search by title or author"
+                               value={query}
+                               onChange={(event) => this.updateQuery(event.target.value)}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {searchRes.map(book => (
+                            <li key={book.id}>
+                                {book.title}
+                            </li>
+                        ))}
+                    </ol>
                 </div>
             </div>
         );
@@ -20,3 +52,11 @@ class Search extends React.Component {
 }
 
 export default Search
+
+// <Book
+// imageUrl={book.imageLinks != null && book.imageLinks.thumbnail}
+// title={book.title}
+// author={book.authors != null && book.authors[0]}
+// shelf={book.shelf}
+// onShelfChanged={(e) => {}} //props.bookShelfChanged(book.id, e.target.value)
+// />
