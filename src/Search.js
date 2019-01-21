d2b2e5d2ab08359c1cb5954fe97189f6a4f697b1
yaoliupeloton <yao.liu@pelotoncycle.com>
2019-01-20 20:14:44 -0500

    search bar

--- src/Search.js
+++ src/Search.js
@@ -3,8 +3,16 @@ import React from 'react';
 class Search extends React.Component {
     render() {
         return (
-            <div>
-                Search Page
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <button className="close-search">Close</button>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author"/>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid"></ol>
+                </div>
             </div>
         );
     }
